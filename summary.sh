bc02b4fda28472e8db50c2b730fbbfdc3d35c2ba
Vladimír Chlup <vchlup@redhat.com>
2024-02-27 15:31:10 +0100

    Add number of non 2xx responses into summary

--- summary.sh
+++ summary.sh
@@ -10,7 +10,16 @@ for v in output/*; do
     echo "-------------------------------------------"
     # print also the header
     echo "              min  mean[+/-sd] median   max"
-    grep -h "Total:" $v/ab-*
+    for t in $v/ab-*; do
+        grep -h "Total:" $t | tr -d '\n'
+        echo -n "    ("
+        if [ $(grep -c "Non-2xx responses" $t) -eq 0 ]; then
+            echo -n "All responses were 2xx   "
+        else
+            grep -h "Non-2xx responses:" $t | tr -d '\n'
+        fi
+        echo ")"
+    done
     echo ""
 done
 
