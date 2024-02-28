a56b6b5bae40936851fb2b74d5ea0801d10a0889
Vladimír Chlup <vchlup@redhat.com>
2024-02-28 08:43:36 +0100

    Add local.sh script for easy running multiple runs locally

---
+++ local.sh
@@ -0,0 +1,15 @@
+# clean previous runs
+rm -rf output/* || true
+rm -rf nodes-*  || true
+sh setup.sh
+
+for count in 2 10 20 50 100 150
+do
+    echo "Running for $count nodes"
+    TOMCAT_COUNT=$count sh run.sh
+    sh summary.sh > summary-for-$count
+    mkdir nodes-$count
+    mv summary-for-$count output/* nodes-$count/
+    echo "Done!"
+done
+
