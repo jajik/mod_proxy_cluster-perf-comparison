d12ff0685cee1e01b9967ad9f6fcb40352a8a7e1
Vladimír Chlup <vchlup@redhat.com>
2024-12-10 16:18:41 +0100

    WIP use g++-12

--- setup.sh
+++ setup.sh
@@ -67,7 +67,7 @@ docker build -t $HTTPD_IMG_1_3 httpd/
 cd ../..
 
 cd client
-CXX=g++ cmake . && make
+CXX=g++-12 cmake . && make
 if [ $? -ne 0 ]; then
     echo "client compilation failed"
     exit 1
