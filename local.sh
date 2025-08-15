e14cf022633b522176c96f5d9098fa184bf00ab6
Vladimír Chlup <vchlup@ibm.com>
2025-08-15 20:42:41 +0200

    Improve naming, tweak printed message

--- local.sh
+++ local.sh
@@ -6,7 +6,7 @@ sh setup.sh
 for count in 2 10 20 50 ## 100 150
 do
     echo "Running for $count nodes"
-    TOMCAT_COUNT=$count sh run.sh
+    TOMCAT_COUNT=$count sh run-suite.sh
     sh summary.sh > summary-for-$count
     mkdir nodes-$count
     mv summary-for-$count output/* nodes-$count/
