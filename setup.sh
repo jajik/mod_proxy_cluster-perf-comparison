5d235363d3475f2397e217845dc43910ef8ecb90
Vladimír Chlup <vchlup@ibm.com>
2025-08-15 20:38:41 +0200

    Rework the testsuite, make it more plugable, unify images creation

--- setup.sh
+++ setup.sh
@@ -1,35 +1,20 @@
 #!/usr/bin/sh
 
-export HTTPD_IMG_1_3=${HTTPD_IMG_1_3:-httpd-mod_proxy_cluster-1.3.x}
-export HTTPD_IMG_2_0=${HTTPD_IMG_2_0:-httpd-mod_proxy_cluster-2.x}
-export IMG=${IMG:-mod_proxy_cluster-testsuite-tomcat}
+# we'll build the tomcat image under our own name
+export IMG=${IMG:-mpc-perfsuite-tomcat}
 
 echo "Setting up dependencies"
 
-echo -n "Replacing tests from mod_proxy_cluster 1.3.x with tests from 2.x..."
-rm -rf mod_cluster-1.3.x/test/
-cp -r mod_proxy_cluster/test/ mod_cluster-1.3.x/test/
-cp Containerfile mod_cluster-1.3.x/test/httpd/
+# First build all the dependencies,
+# then go through all mod_proxy_cluster* directories
+# and build a container image for each of them
 
-sed -i 's|slotmem_shm_module.*modules/mod_slotmem_shm.so|cluster_slotmem_module modules/mod_cluster_slotmem.so|' mod_cluster-1.3.x/test/httpd/mod_proxy_cluster.conf
-sed -i '8s|^|LoadModule proxy_wstunnel_module modules/mod_proxy_wstunnel.so\n|' mod_cluster-1.3.x/test/httpd/mod_proxy_cluster.conf
-echo " Done"
-
-# Increase MaxNode to 50
-for conf_file in mod_proxy_cluster/test/httpd/mod_proxy_cluster.conf mod_cluster-1.3.x/test/httpd/mod_proxy_cluster.conf
-do
-    echo "Maxnode 50"          >> $conf_file
-    echo "ServerLimit 32"      >> $conf_file
-    echo "StartServers 8"      >> $conf_file
-    echo "MaxKeepAliveRequests 0" >> $conf_file
-    if [ $HTTPD_LOG_LEVEL ]; then
-        echo "Setting log level to $HTTPD_LOG_LEVEL"
-        echo "LogLevel $HTTPD_LOG_LEVEL" >> $conf_file
-    fi
-done
-
+#############################
+####  M I S C   A P P S  ####
+#############################
+# Maven installs for the applications used there
 echo -n "Running maven installs... "
-for m in httpd_websocket-testsuite/ mod_cluster-testsuite/ mod_proxy_cluster/test/ mod_cluster-1.3.x/test/ demo-webapp/
+for m in httpd_websocket-testsuite/ mod_cluster-testsuite/ mod_proxy_cluster/test/  demo-webapp/
 do
     cd $m
     mvn install
@@ -39,58 +24,64 @@ do
     fi
     cd $OLDPWD
 done
-
 echo "Done"
 
-echo -n "Creating httpd and tomcat images..."
+###########################
+####    T O M C A T    ####
+###########################
+# Create a tomcat container from the mod_proxy_cluster upstream testsuite
+echo -n "Creating httpd and tomcat images... "
 # First we'll change the default tomcat port from 8080 to 9000 so that we avoid conflicts
 # with the mod_proxy_cluster's port 8090 in case of 10 or more tomcat containers...
-sed -i -e 's/8080/9000/g' mod_proxy_cluster/test/includes/common.sh
-sed -i -e 's/8080/9000/g' mod_proxy_cluster/test/tomcat/server.xml
-
-# tomcat and httpd with mod_proxy_cluster 2.x
 cd mod_proxy_cluster/test/
+sed -i -e 's/8080/9000/g' includes/common.sh
+sed -i -e 's/8080/9000/g' tomcat/server.xml
 # load helper functions
 . includes/common.sh
-# tomcat
 tomcat_create
 if [ $? -ne 0 ]; then
     echo "Error occurred during tomcat image creation"
     exit 1
 fi
+cd $OLDPWD
+echo "Done"
 
-rm -rf httpd/mod_proxy_cluster /tmp/mod_proxy_cluster
-mkdir /tmp/mod_proxy_cluster
-cp -r ../native ../test /tmp/mod_proxy_cluster/
-mv /tmp/mod_proxy_cluster httpd/
-
-docker build -t $HTTPD_IMG_2_0 -f httpd/Containerfile httpd/
-if [ $? -ne 0 ]; then
-    echo "Error occurred during $HTTPD_IMG_2_0 creation"
-    exit 1
-fi
-
-# httpd with mod_proxy_cluster 1.3.x
-cd ../..
-cd mod_cluster-1.3.x/test/
-rm -rf /tmp/mod_proxy_cluster
-mkdir /tmp/mod_proxy_cluster/
-cp -r ../native ../test /tmp/mod_proxy_cluster
-cp -r /tmp/mod_proxy_cluster httpd/
-docker build -t $HTTPD_IMG_1_3 -f httpd/Containerfile httpd/
-if [ $? -ne 0 ]; then
-    echo "Error occurred during $HTTPD_IMG_1_3 creation"
-    exit 1
-fi
-cd ../..
-
+###########################
+####    C L I E N T    ####
+###########################
+echo -n "Compiling a client application... "
 cd client
 cmake . && make
 if [ $? -ne 0 ]; then
     echo "client compilation failed"
     exit 1
 fi
-cd ..
+cd $OLDPWD
+echo "Done"
 
+###########################
+###  MOD_PROXY_CLUSTER  ###
+###########################
+echo "Creating an httpd with mod_proxy_cluster container images from all directories found"
+# create a temporary directory in which we'll build the container images
+mkdir -p .tempdir
+cp Containerfile mod_proxy_cluster.conf httpd-container-run.sh .tempdir/
+for dir in mod_proxy_cluster*/
+do
+    cp -r $dir/native .tempdir/
+    cd .tempdir
+    version=$(echo $dir | sed -rn 's|mod_proxy_cluster-(.*)/|\1|p')
+    # if version is empty, it's our `mod_proxy_cluster` == 2.x version
+    version=${version:-2.x}
+    containername="mpc-perfsuite-mod_proxy_cluster-$version"
+    docker build -t $containername  -f Containerfile .
+    if [ $? -ne 0 ]; then
+        echo "Compilation of mod_proxy_cluster in $dir failed!"
+        exit 1;
+    fi
+    echo "    - $containername built from $dir"
+    cd $OLDPWD
+done
+rm -rf .tempdir
 echo "Done"
 
