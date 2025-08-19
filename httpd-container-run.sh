dc2689816eee9b8facdf4ded5e3699885dbb2004
Vladimír Chlup <vchlup@ibm.com>
2025-08-19 10:50:35 +0200

    Use misspelled EnableMCMPReceive directive for 1.3.x images

--- httpd-container-run.sh
+++ httpd-container-run.sh
@@ -5,6 +5,9 @@ if grep -q "1\.3\." /mpc_version
 then
     sed -i 's|slotmem_shm_module.*modules/mod_slotmem_shm.so|cluster_slotmem_module modules/mod_cluster_slotmem.so|' conf/mod_proxy_cluster.conf
     sed -i '8s|^|LoadModule proxy_wstunnel_module modules/mod_proxy_wstunnel.so\n|' conf/mod_proxy_cluster.conf
+    # Use the misspelled variant for 1.3.x versions, because the correct spelling was added in 1.3.21.Final
+    # and older versions don't support it (and most likely it's not going away in the 1.3.x stream)
+    sed -i 's|EnableMCMPReceive|EnableMCPMReceive|' conf/mod_proxy_cluster.conf
 fi
 
 # Include our mod_proxy_cluster.conf
